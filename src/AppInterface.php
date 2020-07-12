903757cc7d65723281987098207df7782bb967e1
Andrey <alestro@bk.ru>
2020-07-12 17:23:34 +0200

    Create AppInterface.php

---
+++ src/AppInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+
+namespace Bermuda\AppInterface;
+
+
+use Invoker\InvokerInterface;
+use Psr\Container\ContainerInterface;
+use Bermuda\ServiceFactory\FactoryInterface;
+
+
+/**
+ * Interface AppInterface
+ * @package Bermuda\App
+ */
+interface Application extends ContainerInterface, FactoryInterface, InvokerInterface
+{
+    const version = 1.0;
+
+    /**
+     * Run application
+     */
+    public function run(): void ;
+
+    /**
+     * @param mixed $any
+     * @return $this
+     */
+    public function pipe($any): self ;
+
+    /**
+     * @param string $id
+     * @param $value
+     * @return $this
+     */
+    public function set(string $id, $value): self ;
+}
