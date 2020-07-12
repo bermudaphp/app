08a9a9fe8b0afe996e9de8de6729a844f42b6d57
Andrey <alestro@bk.ru>
2020-07-12 18:27:51 +0200

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,126 @@
 <?php
+
+
+namespace Bermuda;
+
+
+/**
+ * @param string $entry
+ * @return AppInterface|mixed
+ */
+function app(string $entry = null)
+{
+    if ($entry != null)
+    {
+        return Registry::get(AppInterface::class)->get($entry);
+    }
+    
+    return Registry::get(AppInterface::class);
+}
+
+/**
+ * @param string $entry
+ * @return mixed
+ */
+function get(string $entry)
+{
+    return Registry::get(AppInterface::class)->get($entry);
+}
+
+/**
+ * @param string $entry
+ * @return mixed
+ */
+function service(string $service): object
+{
+    return Registry::get(AppInterface::class)->get($service);
+}
+
+/**
+ * @param string $template
+ * @param array $params
+ * @return ResponseInterface
+ */
+function view(string $template, array $params = []): ResponseInterface
+{
+    return html_response(service(RendererInterface::class)->render($template, $params));
+}
+
+/**
+ * @param int $code
+ * @param string $reasonPhrase
+ * @return ResponseInterface
+ */
+function response(int $code = 200, string $reasonPhrase = ''): ResponseInterface
+{
+    return service(ResponseFactoryInterface::class)->createResponse($code, $reasonPhrase);
+}
+
+/**
+ * @param string|UriInterface $uri
+ * @param ResponseInterface|null $response
+ * @return ResponseInterface
+ */
+function redirect($uri = '/', ?ResponseInterface $response = null): ResponseInterface
+{
+    if (!is_string($uri) && !$uri instanceof UriInterface)
+    {
+        throw new \InvalidArgumentException(sprintf('Uri provided to %s must be a string or %s instance; received "%s"', __FUNCTION__, UriInterface::class, (is_object($uri) ? get_class($uri) : gettype($uri))));
+    }
+  
+    return ($response ?? response())->withHeader('location', (string) $uri)->withStatusCode(302);
+}
+
+/**
+ * @param string $name
+ * @param array $params
+ * @return string
+ */
+function route(string $name, array $params = []): string
+{
+    return service(GeneratorInterface::class)->generate($name, $params);
+}
+
+/**
+ * @param string $name
+ * @param array $params
+ * @return string
+ */
+function redirect_on_route(string $name, array $params = []) : string
+{
+    return redirect(route($name, $params));
+}
+
+/**
+ * @param string $json
+ * @param ResponseInterface|null $response
+ * @return ResponseInterface
+ */
+function json_response(string $json, ?ResponseInterface $response = null): ResponseInterface
+{
+     if ($response == null)
+     {
+        $response = response();
+     }
+  
+     $response->getBody()->write($json);
+    
+     return $response->withHeader('Content-Type', 'application/json');
+}
+
+/**
+ * @param string $json
+ * @param ResponseInterface|null $response
+ * @return ResponseInterface
+ */
+function html_response(string $html, ?ResponseInterface $response = null): ResponseInterface
+{
+     if ($response == null)
+     {
+        $response = response();
+     }
+  
+     $response->getBody()->write($json);
+    
+     return $response->withHeader('Content-Type', 'text/html');
+}
