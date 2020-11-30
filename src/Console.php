2d7d8c3924c2c79d65ad31c6f97f02c001688ccb
Andrey <inbox.shelamkoff@gmail.com>
2020-11-30 21:56:09 +0300

    Create Console.php

---
+++ src/Console.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Bermuda\App;
+
+
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * Class Console
+ * @package Bermuda\App
+ */
+final class Console extends App
+{
+    private Console\CommandRunnerInterface $runner;
+    private Console\CommandResolverInterface $resolver;
+
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct($container);
+
+        $this->runner = $this->getRunner();
+        $this->resolver = $this->getResolver();
+    }
+
+    /**
+     * @param mixed $any
+     * @return $this|AppInterface
+     */
+    public function pipe($any): AppInterface
+    {
+        $this->runner->add($this->resolver->resolve($any));
+        return $this;
+    }
+
+    public function run(): void
+    {
+        $this->runner->run($this->getInput(), $this->getOutput());
+    }
+
+    /**
+     * @return InputInterface
+     */
+    private function getInput(): InputInterface
+    {
+        return $this->container->has(InputInterface::class)
+            ? $this->container->get(InputInterface::class) :
+            new ArgvInput();
+    }
+
+    /**
+     * @return OutputInterface
+     */
+    private function getOutput(): OutputInterface
+    {
+        return $this->container->has(OutputInterface::class)
+            ? $this->container->get(OutputInterface::class) :
+            new ConsoleOutput();
+    }
+
+    /**
+     * @return Console\CommandRunnerInterface
+     */
+    private function getRunner(): Console\CommandRunnerInterface
+    {
+        return $this->container->has(CommandRunnerInterface::class)
+            ? $this->container->get(CommandRunnerInterface::class) :
+            new Console\SymfonyConsole();
+    }
+
+    /**
+     * @return Console\CommandResolverInterface
+     */
+    private function getResolver(): Console\CommandResolverInterface
+    {
+        return $this->container->has(CommandRunnerInterface::class)
+            ? $this->container->get(CommandRunnerInterface::class) :
+            new Console\CommandResolver($this->container);
+    }
+}
