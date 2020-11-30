a4f3abbc3b90abf2c4156a48e9241297c60f27d1
Andrey <inbox.shelamkoff@gmail.com>
2020-11-30 22:10:49 +0300

    Create CommandRunnerInterface.php

---
+++ src/console/CommandRunnerInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bermuda\App\Console;
+
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+/**
+ * Interface CommandRunnerInterface
+ * @package Bermuda\App\Console
+ */
+interface CommandRunnerInterface
+{
+    /**
+     * @param CommandInterface $command
+     * @return $this
+     */
+    public function add(CommandInterface $command): self;
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     */
+    public function run(InputInterface $input, OutputInterface $output): void ;
+}
