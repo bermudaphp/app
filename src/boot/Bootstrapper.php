09c09c0ae7ceaf02321992b7fdbe3bd597c03333
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2020-12-19 18:37:28 +0300

    Create Bootstrapper.php

---
+++ src/boot/Bootstrapper.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bermuda\App\Boot;
+
+
+use Bermuda\App\AppInterface;
+
+
+/**
+ * Class Bootstrapper
+ * @package Bermuda\App\Boot
+ */
+final class Bootstrapper implements BootstrapperInterface
+{
+    /**
+     * @var BootstrapperInterface[]
+     */
+    private array $bootstrap;
+
+    /**
+     * Bootstrapper constructor.
+     * @param BootstrapperInterface[] $bootstrap
+     */
+    public function __construct(array $bootstrap = [])
+    {
+        foreach ($bootstrap as $bootstrapper)
+        {
+            $this->addBootstrapper($bootstrapper);
+        }
+    }
+
+    /**
+     * @param AppInterface $app
+     */
+    public function boot(AppInterface $app): void
+    {
+        foreach ($this->bootstrap as $bootstrapper)
+        {
+            $bootstrapper->boot($app);
+        }
+    }
+
+    /**
+     * @param BootstrapperInterface $bootstrapper
+     * @return $this
+     */
+    public function addBootstrapper(BootstrapperInterface $bootstrapper): self
+    {
+        $this->bootstrap[] = $bootstrapper;
+        return $this;
+    }
+
+    /**
+     * @param BootstrapperInterface[] $bootstrap
+     * @return static
+     */
+    public static function makeOf(array $bootstrap = []): self
+    {
+        return new self($bootstrap);
+    }
+}
