cff7ca5124e54e794feab9db966a3c7a218ad985
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2020-12-19 18:38:24 +0300

    Create PipelineBootstrapper.php

---
+++ src/boot/PipelineBootstrapper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bermuda\App\Boot;
+
+
+use Bermuda\App\AppInterface;
+
+
+/**
+ * Class PipelineBootstrapper
+ * @package Bermuda\App\Boot
+ */
+final class PipelineBootstrapper implements BootstrapperInterface
+{
+    public function boot(AppInterface $app): void
+    {
+        (static function() use ($app)
+        {
+            require APP_ROOT . '\config\\' . (PHP_SAPI === 'cli' ?
+                    'commands.php' : 'pipeline.php' );
+        })();
+    }
+}
