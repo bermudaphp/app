ef0233dbdf42d6c2ad14243c06eb2d05acbb52fe
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2020-12-19 18:40:44 +0300

    Create RouterBootstrapper.php

---
+++ src/boot/RouterBootstrapper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Bermuda\App\Boot;
+
+
+use Bermuda\App\AppInterface;
+use Bermuda\Router\RouteMap;
+use Bermuda\Router\RouterInterface;
+
+
+/**
+ * Class RouterBootstrapper
+ * @package Bermuda\App\Boot
+ */
+final class RouterBootstrapper implements BootstrapperInterface
+{
+    /**
+     * @param AppInterface $app
+     */
+    public function boot(AppInterface $app): void
+    {
+        PHP_SAPI === 'cli' ?:
+        (static function(RouteMap $routes): void
+        {
+            require APP_ROOT . '/config/routes.php';
+        })($app->get(RouterInterface::class)->getRoutes());
+    }
+}
