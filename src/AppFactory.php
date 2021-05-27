210338b56937e870297ad8d7d8ba6189f7e55bf5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-27 15:14:05 +0300

    Update AppFactory.php

--- src/AppFactory.php
+++ src/AppFactory.php
@@ -16,7 +16,10 @@ final class AppFactory
      */
     public function __invoke(ContainerInterface $container): AppInterface
     {    
-        return $container->has(AppInterface::class) ? $container->get(AppInterface::class)
-            : (php_sapi_name() == 'cli' ? new Console($container): new FastCGI($container));
+        ($app = $container->has(AppInterface::class) ? $container->get(AppInterface::class)
+            : (php_sapi_name() == 'cli' ? new Console($container): new FastCGI($container)))
+            ->boot();
+        
+        return Registry::set(AppInterface::class, $app);
     }
 }
