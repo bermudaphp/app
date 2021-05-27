8002b14fb96ba0b027897ade55865ffa29c181c2
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-27 15:53:25 +0300

    Update FastCGI.php

--- src/FastCGI.php
+++ src/FastCGI.php
@@ -19,24 +19,40 @@ use Bermuda\MiddlewareFactory\MiddlewareFactoryInterface;
 final class FastCGI extends App
 {
     private PipelineInterface $pipeline;
-    private RequestHandlerRunner $runner;
+    private ServerRequestFactory $requestFactory;
+    private ResponseFactoryInterface $responseFactory;
     private MiddlewareFactoryInterface $middlewareFactory;
+    private EmitterInterface $emitter;
 
     public function __construct(ContainerInterface $container)
     {
         parent::__construct($container);
 
-        $this->runner = $container->get(RequestHandlerRunner::class);
+        $this->emitter = $container->get(EmitterInterface::class);
         $this->pipeline = $container->get(PipelineInterface::class);
+        $this->requestFactory = $container->get(ServerRequestFactory::class);
+        $this->responseFactory = $container->get(ResponseFactoryInterface::class);
         $this->middlewareFactory = $container->get(MiddlewareFactoryInterface::class);
     }
 
     /**
      * Run application
+     * @throws ServerRequestException if request handling is failure
+     * @throws \Throwable if request creation is failure
      */
     public function run(): void
     {
-        $this->runner->run();
+        $request = $this->requestFactory->fromGlobals();
+        
+        try
+        {
+            $this->emitter->emit($this->pipeline->handle($request));
+        }
+        
+        catch(\Throwable $e)
+        {
+            throw ServerRequestException::decorate($e, $request);
+        }
     }
 
     /**
