5736cf7af0d87f671db1dbbd96ba06dee84efbbd
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-07-25 23:37:50 +0200

    Create Config.php

---
+++ src/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bermuda\App;
+
+use Laminas\Config as LaminasConfig;
+
+/**
+ * Class Config
+ * @package Bermuda\App
+ */
+class Config extends LaminasConfig implements ConfigInterface
+{
+}
