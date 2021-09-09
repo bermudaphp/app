3d0299819dc6ceb4ae8fa3c4b827690d3a306783
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-09 17:23:50 +0200

    Update Bootstrapper.php

--- src/boot/Bootstrapper.php
+++ src/boot/Bootstrapper.php
@@ -3,45 +3,62 @@
 namespace Bermuda\App\Boot;
 
 use Bermuda\App\AppInterface;
-use Bermuda\Registry\Registry;
+use Bermuda\ErrorHandler\LogErrorListener;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 
 final class Bootstrapper implements BootstrapperInterface
 {
-    /**
-     * @var BootstrapperInterface[]
-     */
-    private array $bootstrap = [];
+    public function __construct(private ?iterable $bootstrap = [])
+    {
+    }
 
     /**
      * @inerhitDoc
      */
     public function boot(AppInterface $app): void
     {
-        foreach ($this->bootstrap as $bootstrapper) $bootstrapper->boot($app);
+        foreach ($this->bootstrap as $bootstrapper) {
+            if (!$bootstrapper instanceof BootstrapperInterface) {
+                $bootstrapper = $app->get($bootstrapper);
+            }
+
+            $bootstrapper->boot($app);
+        }
     }
 
     /**
      * @param BootstrapperInterface $bootstrapper
-     * @return $this
+     * @return self
      */
-    public function addBootstrapper(BootstrapperInterface $bootstrapper): self
+    public function add(BootstrapperInterface $bootstrapper): self
     {
         $this->bootstrap[] = $bootstrapper;
         return $this;
     }
 
     /**
-     * @param BootstrapperInterface[] $bootstrap
+     * @param array $bootstrap
      * @return self
      */
-    public static function makeOf(iterable $bootstrap = []): self
+    public function merge(array $bootstrap): self
     {
-        $bootstrapper = new self;
-        
-        foreach ($bootstrap as $i) {
-            $bootstrapper->addBootstrapper($i);
-        }
-        
-        return $bootstrapper;
+        return array_merge($this->bootstrap, $bootstrap);
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public static function makeDefault(ContainerInterface $container): self
+    {
+        return new self([
+            new RouterBootstrapper(),
+            new PipelineBootstrapper(),
+            new ErrorHandlerBootstrapper([
+                (new LogErrorListener($container->get(LoggerInterface::class)))
+                    ->except('Bermuda\Router\Exception\RouteNotFoundException')
+                    ->except('Bermuda\Router\Exception\MethodNotAllowedException')
+            ])
+        ]);
     }
 }
