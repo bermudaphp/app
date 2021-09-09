ed4c39fd0ec95483ffd5c3304cf51a3e937addc5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-09 15:53:20 +0200

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -2,109 +2,165 @@
 
 namespace Bermuda\App;
 
+use RuntimeException;
+use Bermuda\Arrayable;
+use Countable, Iterator, ArrayAccess;
+
 final class Config implements Countable, Iterator, ArrayAccess, Arrayable
-{  
+{
     private array $data;
-    
+
     public function __construct(iterable $data)
     {
-        $array = [];
-        
-        foreach($data as $k => $v)
-        {
-            $array[$k] = is_iterable($v) ? new Config($v) : $v ;
+        foreach ($data as $key => $value) {
+            $this->data[$key] = is_iterable($value) 
+                ? new Config($value) : $value;
         }
-        
-        $this->data = $array ;
     }
-  
-    public function get($name, $default = null, bool $invoke = true)
-    {
-        return array_key_exists($name, $this->data) ? $this->data[$name]
-          : ($invoke && is_callable($default) ? $default() : $default)
-    }
-  
+
+    /**
+     * @param $name
+     * @return mixed|self
+     */
     public function __get($name)
     {
         return $this->offsetGet($name);
     }
 
+    /**
+     * @throws RuntimeException
+     */
     public function __set($name, $value)
     {
         $this->offsetSet($name, $value);
     }
-    
-    public function toArray(): array
-    {
-        $array = [];
-        $data  = $this->data;
 
-        foreach ($data as $key => $value)
-        {
-            $array[$key] = $value instanceof Config
-              ? $value->toArray() : $value ;
-        }
-
-        return $array;
-    }
-
-    public function __isset($name): bool
-    {
-        return $this->offsetExists($name);
-    }
-
-    public function __unset($name): void
-    {
-        $this->offsetUnset($name);
-    }
-
-    public function count(): int
-    {
-        return count($this->data);
-    }
-
-    public function current()
-    {
-        return current($this->data);
-    }
-
-    public function key()
-    {
-        return key($this->data);
-    }
-
-    public function next()
-    {
-        next($this->data);
-    }
-
-    public function rewind()
-    {
-        reset($this->data);
-    }
-
-    public function valid()
-    {
-        return ($this->key() !== null);
-    }
-
-    public function offsetExists($offset)
-    {
-        return array_key_exists($name, $this->data);
-    }
-
+    /**
+     * @inerhitDoc
+     */
     public function offsetGet($offset)
     {
         return $this->get($offset);
     }
 
-    public function offsetSet($offset, $value)
+    /**
+     * @param $offset
+     * @param null $default
+     * @param bool $invoke
+     * @return self|null|mixed
+     */
+    public function get($offset, $default = null, bool $invoke = true)
     {
-         throw new \RuntimeException('Config is not immutable');
+        return $this->offsetExists($offset) ? $this->data[$offset]
+            : ($invoke && is_callable($default) ? $default() : $default);
     }
 
+    /**
+     * @throws RuntimeException
+     */
+    public function offsetSet($offset, $value)
+    {
+        throw $this->notImmutable();
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function toArray(): array
+    {
+        $array = [];
+
+        foreach ($this->data as $key => $value) {
+            $array[$key] = $value instanceof Config
+                ? $value->toArray() : $value;
+        }
+
+        return $array;
+    }
+
+    /**
+     * @param $name
+     * @return bool
+     */
+    public function __isset($name): bool
+    {
+        return $this->offsetExists($name);
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function offsetExists($offset): bool
+    {
+        return array_key_exists($offset, $this->data);
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    public function __unset($name): void
+    {
+        $this->offsetUnset($name);
+    }
+
+    /**
+     * @throws RuntimeException
+     */
     public function offsetUnset($offset)
     {
-         throw new \RuntimeException('Config is not immutable');
+        throw $this->notImmutable();
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function count(): int
+    {
+        return count($this->data);
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function current()
+    {
+        return current($this->data);
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function next()
+    {
+        next($this->data);
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function rewind()
+    {
+        reset($this->data);
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function valid(): bool
+    {
+        return ($this->key() !== null);
+    }
+
+    /**
+     * @inerhitDoc
+     */
+    public function key(): float|bool|int|string|null
+    {
+        return key($this->data);
+    }
+
+    private function notImmutable(): RuntimeException
+    {
+        return new RuntimeException('Config is not immutable');
     }
 }
