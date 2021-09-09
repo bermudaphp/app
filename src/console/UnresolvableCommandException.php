c6e6d8cc7cc289054bdf762ccf6f0231cb88fb3e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-09 16:42:27 +0200

    Update UnresolvableCommandException.php

--- src/console/UnresolvableCommandException.php
+++ src/console/UnresolvableCommandException.php
@@ -2,40 +2,35 @@
 
 namespace Bermuda\App\Console;
 
-use Throwable;
+use RuntimeException;
 
-/**
- * Class UnresolvableCommandException
- * @package Bermuda\App\Console
- */
-final class UnresolvableCommandException extends \RuntimeException
+final class UnresolvableCommandException extends RuntimeException
 {
-    private $command;
-    
+    private mixed $command;
+
     public function __construct(?string $message = null, $command = null)
     {
         $this->command = $command;
-        
-        if (!$message && is_string($command))
-        {
+
+        if (!$message && is_string($command)) {
             $message = 'Unresolvable command: ' . $command;
         }
 
         parent::__construct($message ?? 'Unresolvable command');
     }
-    
-    public function getCommand()
-    {
-        return $this->command;
-    }
-    
+
     public static function reThrow(UnresolvableCommandException $e, array $backtrace): void
     {
         $self = new self($e->getMessage(), $e->getCommand());
-        
+
         $self->file = $backtrace['file'];
         $self->line = $backtrace['line'];
-        
+
         throw $self;
+    }
+
+    public function getCommand()
+    {
+        return $this->command;
     }
 }
