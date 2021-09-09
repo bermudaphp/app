f732aa340621bb5a77ea4f097864942039a4cace
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-09 16:09:19 +0200

    Update AppException.php

--- src/AppException.php
+++ src/AppException.php
@@ -2,12 +2,17 @@
 
 namespace Bermuda\App;
 
-use Throwable;
+use RuntimeException;
 
-class AppException extends \RuntimeException
+class AppException extends RuntimeException
 {
-    public static function runned(): self
+    public static function isRun(): self
     {
-        return new static('Application already runned.');
+        return new static('Application already run');
+    }
+
+    public static function entryExists($id): self
+    {
+        return new static(sprintf('Entry with id: %s already exists in the container', $id));
     }
 }
