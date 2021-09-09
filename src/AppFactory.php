ce04cd75242caf5d0c585a6c4d7249870ca20e74
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-09 16:11:03 +0200

    Update AppFactory.php

--- src/AppFactory.php
+++ src/AppFactory.php
@@ -4,11 +4,21 @@ namespace Bermuda\App;
 
 use Bermuda\Registry\Registry;
 use Psr\Container\ContainerInterface;
-
-use function Bermuda\{is_console_sapi, cget};
+use function Bermuda\{cget, is_console_sapi};
 
 final class AppFactory
 {
+    /**
+     * @param ContainerInterface $container
+     * @return AppInterface
+     */
+    public static function make(ContainerInterface $container): AppInterface
+    {
+        $app = cget($container, AppInterface::class, static fn() => (new AppFactory)($container));
+        cget($app, Boot\BootstrapperInterface::class, null)?->boot($app);
+        return Registry::set(AppInterface::class, $app);
+    }
+
     /**
      * @param ContainerInterface $container
      * @return AppInterface
@@ -17,16 +27,5 @@ final class AppFactory
     {
         return is_console_sapi() ? Console::makeFrom($container)
             : Server::makeFrom($container);
-    }
-
-    /**
-     * @param ContainerInterface $container
-     * @return AppInterface
-     */
-    public static function make(ContainerInterface $container): AppInterface
-    {
-        $app = cget($container, AppInterface::class, static fn() => (new AppFactory)($container)));
-        cget($app, BootstrapperInterface::class, null)?->boot($app);
-        return Registry::set(AppInterface::class, $app);
     }
 }
