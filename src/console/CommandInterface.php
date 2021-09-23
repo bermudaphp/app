4820e8c834b0c343f6a53fcfed3e6763c0e40f55
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-23 19:34:53 +0200

    Update CommandInterface.php

--- src/console/CommandInterface.php
+++ src/console/CommandInterface.php
@@ -2,18 +2,16 @@
 
 namespace Bermuda\App\Console;
 
-use Symfony\Component\Console\{
-    Input\InputInterface, 
-    Output\OutputInterface
-};
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 interface CommandInterface
 {
-    public const success = 1;
-    public const failure = 0;
+    public const success = Command::SUCCESS;
+    public const failure = Command::FAILURE;
 
     public function getName(): string ;
-    public function getDescription(): string;
-
-    public function execute(InputInterface $input, OutputInterface $output): int ;
+    public function getDescription(): string ;
+    public function __invoke(InputInterface $input, OutputInterface $output): int ;
 }
