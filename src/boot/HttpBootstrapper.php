980e69fb0db5977dc0fd19cf7f747aae9e08f57e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-03 13:51:08 +0200

    Update HttpBootstrapper.php

--- src/boot/HttpBootstrapper.php
+++ src/boot/HttpBootstrapper.php
@@ -4,6 +4,8 @@ namespace Bermuda\App\Boot;
 
 use Bermuda\HTTP\Responder;
 use Bermuda\App\AppInterface;
+use Bermuda\Router\Generator;
+use Bermuda\Router\Router;
 use Bermuda\String\Json;
 use Bermuda\String\Stringable;
 
@@ -28,6 +30,11 @@ final class HttpBootstrapper implements BootstrapperInterface
         
         $app->registerCallback('redirect', static function(string|UriInterface $location, bool $permanent = false) use ($app): ResponseInterface {
             return $app->get(Responder::class)->redirect($location, $permanent);
+        });
+
+        $app->registerCallback('route', static function(string $name, array $params = [], bool $redirect = false) use ($app): string|ResponseInterface {
+            $path = $app->router->generate($name, $params);
+            return $redirect ? $app->redirect($path) : $path;
         });
     }
 }
