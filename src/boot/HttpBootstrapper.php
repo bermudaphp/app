d662820b6617b0d3a1a128631617688b5e213cce
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-10-05 20:37:29 +0200

    Update HttpBootstrapper.php

--- src/boot/HttpBootstrapper.php
+++ src/boot/HttpBootstrapper.php
@@ -19,17 +19,12 @@ final class HttpBootstrapper implements BootstrapperInterface
      */
     public function boot(AppInterface $app): void
     {
-        $app->registerCallback('respond', static function(int $code = 200, $content = null) use ($app): ResponseInterface {
-
-            if (!is_string($content) && !$content instanceof Stringable) {
-                $content = Json::encode($content);
-            }
-
-            return $app->get(Responder::class)->respond($code, $content);
+        $app->registerCallback('respond', static function(int $code = 200, $content = null, string $contentType = null) use ($app): ResponseInterface {
+            return $app->get(Responder::class)->respond($code, $content, $contentType);
         });
         
-        $app->registerCallback('redirect', static function(string|UriInterface $location, bool $permanent = false) use ($app): ResponseInterface {
-            return $app->get(Responder::class)->redirect($location, $permanent);
+        $app->registerCallback('redirect', static function(string|UriInterface $location, int $code = 302) use ($app): ResponseInterface {
+            return $app->get(Responder::class)->redirect($location, $code);
         });
 
         $app->registerCallback('route', static function(string $name, array $params = [], bool $redirect = false) use ($app): string|ResponseInterface {
