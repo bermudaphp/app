b6462429cd8b85a07432fb07997de9f9da5edeb8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-23 20:00:43 +0200

    Update Console.php

--- src/Console.php
+++ src/Console.php
@@ -2,39 +2,24 @@
 
 namespace Bermuda\App;
 
-use Bermuda\ErrorHandler\ErrorHandlerInterface;
-use Bermuda\ServiceFactory\FactoryInterface as ServiceFactoryInterface;
+use DI\FactoryInterface;
 use Invoker\InvokerInterface;
 use Psr\Container\ContainerInterface;
+use Bermuda\ErrorHandler\ErrorHandlerInterface;
 use Symfony\Component\Console\Input\{ArgvInput, InputInterface};
 use Symfony\Component\Console\Output\{ConsoleOutput, OutputInterface};
-use function Bermuda\Config\cget;
 
 final class Console extends App
 {
-    private Console\CommandRunnerInterface $runner;
-    private Console\CommandResolverInterface $resolver;
-
     public function __construct(ContainerInterface      $container, InvokerInterface $invoker,
-                                ServiceFactoryInterface $serviceFactory, ErrorHandlerInterface $errorHandler
+                                FactoryInterface $factory, ErrorHandlerInterface $errorHandler,
+                                private Console\CommandRunnerInterface $runner,
+                                private Console\CommandResolverInterface $resolver
     )
     {
-        parent::__construct($container, $invoker, $serviceFactory, $errorHandler);
-
-        $this->runner = self::getRunner($this);
-        $this->resolver = self::getResolver($this);
+        parent::__construct($container, $invoker, $factory, $errorHandler);
     }
-
-    private static function getRunner(self $console): Console\CommandRunnerInterface
-    {
-        return cget($console, Console\CommandRunnerInterface::class, fn() => new Console\SymfonyConsole, true);
-    }
-
-    private static function getResolver(self $console): Console\CommandResolverInterface
-    {
-        return cget($console, Console\CommandResolverInterface::class, fn() => new Console\CommandResolver($console), true);
-    }
-
+    
     /**
      * @inheritDoc
      */
@@ -51,16 +36,6 @@ final class Console extends App
 
     protected function doRun(): void
     {
-        $this->runner->run(self::getInput($this), self::getOutput($this));
-    }
-
-    private static function getInput(self $console): InputInterface
-    {
-        return cget($console, InputInterface::class, static fn() => new ArgvInput, true);
-    }
-
-    private function getOutput(self $console): OutputInterface
-    {
-        return cget($console, OutputInterface::class, static fn() => new ConsoleOutput, true);
+        $this->runner->run($this->get(InputInterface::class), $this->get(OutputInterface::class));
     }
 }
