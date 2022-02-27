99a9b83d94c0af490def09ce7b15da5b7849dac1
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-27 17:19:51 +0200

    Update ErrorHandlerBootstrapper.php

--- src/boot/ErrorHandlerBootstrapper.php
+++ src/boot/ErrorHandlerBootstrapper.php
@@ -3,38 +3,30 @@
 namespace Bermuda\App\Boot;
 
 use Bermuda\App\AppInterface;
-use Bermuda\ErrorHandler\{ErrorHandler, ErrorListenerInterface};
+use Bermuda\ErrorHandler\{ErrorHandler, ErrorListenerInterface, ConfigProvider};
 
 final class ErrorHandlerBootstrapper implements BootstrapperInterface
 {
-    public function __construct(private iterable $listeners = [])
-    {
-        $this->listeners = $listeners;
-    }
-
-    /**
-     * @param ErrorListenerInterface[] $listeners
-     * @return $this
-     */
-    public function setListeners(iterable $listeners): self
-    {
-        $this->listeners = $listeners;
-        return $this;
-    }
-    
     /**
      * @inerhitDoc 
      */
     public function boot(AppInterface $app): AppInterface
     {
         $handler = $app->get(ErrorHandler::class);
-        foreach ($this->getListeners($app) as $listener) $handler->on($listener);
+        foreach ($this->getListeners($app) as $l) $handler->on($l);
         return $app;
     }
    
     private function getListeners(AppInterface $app): \Generator
     {
-        foreach ($this->listeners as $listener) yield !$listener instanceof ErrorListenerInterface 
-            ? $app->get($listener) : $listener ;
+        $config = $app->config['error'];
+        
+        if ($config !== null && is_iterable($config['error.listeners'])) {
+            foreach($config['error.listeners'] as $l) {
+                yield $l instanceof ErrorListenerInterface ? $l : $app->get($l);
+            }
+        }
+        
+        yield;
     }
 }
