dee5288ba31ccefee37b6a3729a7b3321814434c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-27 17:20:06 +0200

    Update ErrorHandlerBootstrapper.php

--- src/boot/ErrorHandlerBootstrapper.php
+++ src/boot/ErrorHandlerBootstrapper.php
@@ -20,7 +20,6 @@ final class ErrorHandlerBootstrapper implements BootstrapperInterface
     private function getListeners(AppInterface $app): \Generator
     {
         $config = $app->config['error'];
-        
         if ($config !== null && is_iterable($config['error.listeners'])) {
             foreach($config['error.listeners'] as $l) {
                 yield $l instanceof ErrorListenerInterface ? $l : $app->get($l);
