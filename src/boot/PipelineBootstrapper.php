a2715143bbc214633fcb3061a2480021fe19204e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-04-08 13:19:19 +0200

    Update PipelineBootstrapper.php

--- src/boot/PipelineBootstrapper.php
+++ src/boot/PipelineBootstrapper.php
@@ -3,6 +3,7 @@
 namespace Bermuda\App\Boot;
 
 use Bermuda\App\AppInterface;
+use const Bermuda\App\is_cli;
 
 final class PipelineBootstrapper implements BootstrapperInterface
 {
@@ -11,7 +12,7 @@ final class PipelineBootstrapper implements BootstrapperInterface
      */
     public function boot(AppInterface $app): AppInterface
     {
-        require '.\config' . (\Bermuda\App\is_cli ? '\commands.php' : '\pipeline.php' );
+        require '.\config' . (is_cli ? '\commands.php' : '\pipeline.php' );
         return $app;
     }
 }
