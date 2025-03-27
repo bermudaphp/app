d1be1f7de8fd1e8da01f28030606a85ebc312b77
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-27 02:49:25 +0200

    Update DateTime.php

--- src/boot/DateTime.php
+++ src/boot/DateTime.php
@@ -4,13 +4,14 @@ namespace Bermuda\App\Boot;
 
 use Bermuda\Clock\Clock;
 use Bermuda\App\AppInterface;
+use Bermuda\Config\Config;
 
 final class DateTime implements Bootable
 {
     public function boot(AppInterface $app): void
     {
-        Clock::timeZone($app->config->timeZone);
-        Clock::locale($app->config->locale);
+        //Clock::timeZone($app->config->timeZone);
+        //Clock::locale($app->config->locale);
 
         $app->registerCallback('createDate', '\Bermuda\Clock\Clock::create');
 
@@ -19,7 +20,7 @@ final class DateTime implements Bootable
         $timezone = $app->config[Config::app_timezone]
             ?? date_default_timezone_get();
 
-        Clock::timeZone(new DateTimeZone($timezone));
+        Clock::timeZone(new \DateTimeZone($timezone));
         date_default_timezone_set($timezone);
     }
 }
