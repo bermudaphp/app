80bb421c2768d5858652e305b7fd36eff7c389af
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-27 03:45:07 +0200

    Update CompiledServer.php

--- src/compile/CompiledServer.php
+++ src/compile/CompiledServer.php
@@ -2,10 +2,12 @@
 
 namespace Bermuda\App\Compile;
 
+use Bermuda\App\AppInterface;
 use Bermuda\App\Trait\ServerApp;
 use DI\CompiledContainer;
+use Psr\Http\Server\RequestHandlerInterface;
 
-class CompiledServer extends CompiledContainer
+class CompiledServer extends CompiledContainer implements AppInterface, RequestHandlerInterface
 {
     use ServerApp;
 }
