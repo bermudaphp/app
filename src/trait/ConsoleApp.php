0659e4b9286e86d1d0987a5674a3aebea464b695
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-03-29 19:31:30 +0200

    Update ConsoleApp.php

--- src/trait/ConsoleApp.php
+++ src/trait/ConsoleApp.php
@@ -3,24 +3,27 @@
 namespace Bermuda\App\Trait;
 
 use Bermuda\App\AppInterface;
+use Bermuda\App\Console\UnresolvableCommandException;
 use DI\Definition\Source\MutableDefinitionSource;
 use DI\Proxy\ProxyFactory;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Bermuda\App\Console\CommandRunnerInterface;
+use Bermuda\App\Console\CommandResolverInterface;
 
 trait ConsoleApp
 {
     use App { bindEntries as bindEntriesSuper; }
 
-    protected Console\CommandRunnerInterface $runner;
-    protected Console\CommandResolverInterface $resolver;
+    protected CommandRunnerInterface $runner;
+    protected CommandResolverInterface $resolver;
 
     protected function bindEntries(): void
     {
         $this->bindEntriesSuper();
-        $this->runner = $this->get(Console\CommandRunnerInterface::class);
-        $this->resolver = $this->get(Console\CommandResolverInterface::class);
+        $this->runner = $this->get(CommandRunnerInterface::class);
+        $this->resolver = $this->get(CommandResolverInterface::class);
     }
 
     /**
@@ -30,8 +33,8 @@ trait ConsoleApp
     {
         try {
             $this->runner->add($this->resolver->resolve($any));
-        } catch (Console\UnresolvableCommandException $e) {
-            Console\UnresolvableCommandException::reThrow($e, debug_backtrace()[0]);
+        } catch (UnresolvableCommandException $e) {
+            UnresolvableCommandException::reThrow($e, debug_backtrace()[0]);
         }
 
         return $this;
