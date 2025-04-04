136b96664d47e71b0f5e3f709099e66705613bc7
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2025-04-04 14:43:17 +0200

    Update BootableCollector.php

--- src/boot/BootableCollector.php
+++ src/boot/BootableCollector.php
@@ -5,8 +5,25 @@ namespace Bermuda\App\Boot;
 use Bermuda\App\AppInterface;
 use Psr\Container\ContainerInterface;
 
+use function Bermuda\Config\conf;
+
 final class BootableCollector implements Bootable
 {
+    public const string CONFIG_KEY_BOOTABLES = 'Bermuda\App\Boot\Bootable:bootables';
+    public const string CONFIG_KEY_EXCLUDE = 'Bermuda\App\Boot\Bootable:exclude';
+    
+    private const array defaults = [
+        PathHelper::class,
+        Routing::class,
+        Finder::class,
+        Pipeline::class,
+        ErrorHandling::class,
+        Http::class,
+        Configurator::class,
+        DateTime::class,
+        Renderer::class
+    ];
+
     /**
      * @var Bootable[]
      */
@@ -30,6 +47,30 @@ final class BootableCollector implements Bootable
         $this->bootables[] = $bootable;
         return $this;
     }
+    
+    public static function createFromContainer(ContainerInterface $container): self
+    {
+        $bootable = array_merge(self::defaults,
+            ($config = conf($container))->get(self::CONFIG_KEY_BOOTABLES, [])
+        );
+        
+        $self = new self();
+        $exclude = $config->get($self::CONFIG_KEY_EXCLUDE, []);
+        
+        if ($exclude !== []) {
+            foreach ($bootable as $bootable) {
+                if (!in_array($bootable, $exclude)) $self->add(new $bootable);
+            }
+            
+            return $self;
+        }
+        
+        foreach ($bootable as $bootable) {
+            $self->add(new $bootable);
+        }
+        
+        return $self;
+    }
 
     /**
      * @inerhitDoc
@@ -37,7 +78,9 @@ final class BootableCollector implements Bootable
     public static function withDefaults(ContainerInterface $container): self
     {
         return new self([
+            new PathHelper,
             new Routing,
+            new Finder,
             new Pipeline,
             new ErrorHandling,
             new Http,
